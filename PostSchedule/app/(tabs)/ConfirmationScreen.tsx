616f26a86dd9a09f8f27b93f20d54847cbfaded1
Keshia Poedjiono <keshiapud@gmail.com>
2025-04-26 01:40:35 -0400

    Create confirmation page (dropdown list still not working)

---
+++ PostSchedule/app/(tabs)/ConfirmationScreen.tsx
@@ -0,0 +1,150 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import RNPickerSelect from 'react-native-picker-select';
+
+const ConfirmationScreen = () => {
+  const router = useRouter();
+  const [recipient, setRecipient] = React.useState<string | null>(null);
+  const [status, setStatus] = React.useState<string | null>(null);
+
+  const handleRecipientChange = (value: string) => {
+    setRecipient(value);
+  };
+
+  const handleStatusChange = (value: string) => {
+    setStatus(value);
+  };
+
+  const handleFinishPress = () => {
+    // handle submission
+  };
+
+  const recipientOption = [
+    { label: 'All', value: 'all' },
+    { label: 'Custom', value: 'custom' },
+  ];
+
+  const statusOption = [
+    { label: 'Draft', value: 'draft' },
+    { label: 'Post Now', value: 'post' },
+    { label: 'Scheduled', value: 'scheduled' },
+  ];
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Text style={styles.title}>Confirm</Text>
+        <View style={styles.formContainer}>
+          <Text style={styles.formTitle}>Choose Recipient {recipient}</Text>
+          <RNPickerSelect
+            onValueChange={handleRecipientChange}
+            items={recipientOption}
+            style={pickerSelectStyles}
+            placeholder={{ label: 'Select an option...', value: null }}
+            value={recipient}
+          />
+
+          <Text style={styles.formTitle}>Status {status}</Text>
+          <RNPickerSelect
+            onValueChange={handleStatusChange}
+            items={statusOption}
+            style={pickerSelectStyles}
+            placeholder={{ label: 'Select an option...', value: null }}
+            value={status}
+          />
+        </View>
+
+        <Pressable
+          onPress={handleFinishPress}
+          style={({ pressed }) => [
+            styles.finishButton,
+            {
+              backgroundColor: pressed ? '#505050' : 'rgba(39, 39, 39, 1)',
+            },
+          ]}
+        >
+          <Text style={styles.buttonText}>Finish</Text>
+        </Pressable>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'flex-start',
+    backgroundColor: '#FE0E0E0',
+  },
+  title: {
+    fontSize: 30,
+    margin: 20,
+    textAlign: 'center',
+    paddingTop: 80,
+  },
+  formContainer: {
+    marginBottom: 30,
+  },
+  formTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  finishButton: {
+    height: 50,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    color: 'black',
+    paddingRight: 30,
+  },
+  inputIOSContainer: {
+    marginVertical: 10,
+    width: '100%',
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    color: 'black',
+    paddingRight: 30,
+    width: '100%',
+  },
+});
+
+export default ConfirmationScreen;
